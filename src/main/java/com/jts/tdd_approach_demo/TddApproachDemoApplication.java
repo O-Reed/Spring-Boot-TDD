605d88a72318ef4e3e41ab4daa21babead400f30
O-Reed <cooldev0822@gmail.com>
2025-01-12 07:40:21 -0500

    first commit

---
+++ src/main/java/com/jts/tdd_approach_demo/TddApproachDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jts.tdd_approach_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddApproachDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddApproachDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jts/tdd_approach_demo/TddApproachDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jts.tdd_approach_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddApproachDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
